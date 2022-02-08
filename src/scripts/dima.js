140f51fcf2221f580f4ea7fea1c77f4de9108578
Evgeniy Martynenko <marttynenko@gmail.com>
2022-02-08 23:45:20 +0300

    multi swipers fix

--- src/scripts/dima.js
+++ src/scripts/dima.js
@@ -1,21 +1,29 @@
 // Слайдер
 (function () {
-  const toggler = document.querySelectorAll('.ui-swiper')
+  const toggler = document.querySelectorAll('.node-slider')
   if (!toggler.length) return
 
   toggler.forEach((el,index) => {
-    const ds = `ui-swiper-${index}`
-    el.classList.add(ds)
+    const slider = el.querySelector('.ui-swiper')
+    const prevArrow = el.querySelector('.swiper-button-prev')
+    const nextArrow = el.querySelector('.swiper-button-next')
 
-    new Swiper(el, {
+    const ds = `ui-swiper-${index}`
+    slider.classList.add(ds)
+
+    prevArrow.classList.add(`swiper-button-prev-${index}`)
+    nextArrow.classList.add(`swiper-button-next-${index}`)
+
+
+    const swiper = new Swiper(`.${ds}`, {
       autoHeight: true,
       pagination: {
         el: ".swiper-pagination",
         type: "fraction",
       },
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: nextArrow,
+        prevEl: prevArrow,
       },
     })
   })
