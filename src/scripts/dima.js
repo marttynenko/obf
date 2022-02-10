6515bf4b5043a9c29a1135f815394ab487b81394
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-02-10 15:32:09 +0100

    museum-page(на проверку)
    что-то мне подсказывает, что будешь бить

--- src/scripts/dima.js
+++ src/scripts/dima.js
@@ -1,19 +1,18 @@
 // Слайдер
 (function () {
-  const toggler = document.querySelectorAll('.node-slider')
-  if (!toggler.length) return
+  const toggler = document.querySelectorAll(".node-slider");
+  if (!toggler.length) return;
 
-  toggler.forEach((el,index) => {
-    const slider = el.querySelector('.ui-swiper')
-    const prevArrow = el.querySelector('.swiper-button-prev')
-    const nextArrow = el.querySelector('.swiper-button-next')
+  toggler.forEach((el, index) => {
+    const slider = el.querySelector(".ui-swiper");
+    const prevArrow = el.querySelector(".swiper-button-prev");
+    const nextArrow = el.querySelector(".swiper-button-next");
 
-    const ds = `ui-swiper-${index}`
-    slider.classList.add(ds)
+    const ds = `ui-swiper-${index}`;
+    slider.classList.add(ds);
 
-    prevArrow.classList.add(`swiper-button-prev-${index}`)
-    nextArrow.classList.add(`swiper-button-next-${index}`)
-
+    prevArrow.classList.add(`swiper-button-prev-${index}`);
+    nextArrow.classList.add(`swiper-button-next-${index}`);
 
     const swiper = new Swiper(`.${ds}`, {
       autoHeight: true,
@@ -25,8 +24,14 @@
         nextEl: nextArrow,
         prevEl: prevArrow,
       },
-    })
-  })
-  
+    });
+  });
 })();
 
+let on = document.querySelector(".museum").classList.contains("museum");
+
+if (on == true) {
+  document.querySelector(".page").style.paddingTop = 0;
+  document.querySelector(".wrapper").style.paddingTop = 0;
+  document.querySelector(".header").style.background = "transparent";
+}
