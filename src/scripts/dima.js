b40f3aea5473970830636e69f3eaa51e5e7465e0
NazarJS <nazar.poluyanow@gmail.com>
2022-02-21 10:31:27 +0300

    pull

--- src/scripts/dima.js
+++ src/scripts/dima.js
@@ -1,18 +1,19 @@
 // Слайдер
 (function () {
-  const toggler = document.querySelectorAll(".node-slider");
-  if (!toggler.length) return;
+  const toggler = document.querySelectorAll('.node-slider')
+  if (!toggler.length) return
 
-  toggler.forEach((el, index) => {
-    const slider = el.querySelector(".ui-swiper");
-    const prevArrow = el.querySelector(".swiper-button-prev");
-    const nextArrow = el.querySelector(".swiper-button-next");
+  toggler.forEach((el,index) => {
+    const slider = el.querySelector('.ui-swiper')
+    const prevArrow = el.querySelector('.swiper-button-prev')
+    const nextArrow = el.querySelector('.swiper-button-next')
 
-    const ds = `ui-swiper-${index}`;
-    slider.classList.add(ds);
+    const ds = `ui-swiper-${index}`
+    slider.classList.add(ds)
 
-    prevArrow.classList.add(`swiper-button-prev-${index}`);
-    nextArrow.classList.add(`swiper-button-next-${index}`);
+    prevArrow.classList.add(`swiper-button-prev-${index}`)
+    nextArrow.classList.add(`swiper-button-next-${index}`)
+
 
     const swiper = new Swiper(`.${ds}`, {
       autoHeight: true,
@@ -24,26 +25,8 @@
         nextEl: nextArrow,
         prevEl: prevArrow,
       },
-    });
-  });
+    })
+  })
+  
 })();
 
-if (document.querySelector(".ui-page")) {
-  let wrap = document.querySelector(".wrapper");
-  let uihead = document.querySelector(".header");
-  wrap.classList.add("ui-wrapper");
-  uihead.classList.add("ui-header");
-}
-
-if (document.querySelector(".museum")) {
-  let wrap = document.querySelector(".wrapper");
-  let uihead = document.querySelector(".header");
-  wrap.classList.add("wrapper-museum");
-  uihead.classList.add("ui-header");
-}
-
-// if (window.pageYOffset > 0) {
-//   let wrap = document.querySelector(".wrapper");
-//   wrap.classList.remove("ui-wrapper");
-//   document.querySelector(".header").style.background = "#222E3F";
-// }
