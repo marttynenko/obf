6e04569d04c014e15bec20213acccf0e5fcc3c93
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-02-25 14:12:06 +0100

    zxc
    zxc

--- src/scripts/dima.js
+++ src/scripts/dima.js
@@ -1,19 +1,18 @@
 // Слайдер
 (function () {
-  const toggler = document.querySelectorAll('.node-slider')
-  if (!toggler.length) return
+  const toggler = document.querySelectorAll(".node-slider");
+  if (!toggler.length) return;
 
-  toggler.forEach((el,index) => {
-    const slider = el.querySelector('.ui-swiper')
-    const prevArrow = el.querySelector('.swiper-button-prev')
-    const nextArrow = el.querySelector('.swiper-button-next')
+  toggler.forEach((el, index) => {
+    const slider = el.querySelector(".ui-swiper");
+    const prevArrow = el.querySelector(".swiper-button-prev");
+    const nextArrow = el.querySelector(".swiper-button-next");
 
-    const ds = `ui-swiper-${index}`
-    slider.classList.add(ds)
+    const ds = `ui-swiper-${index}`;
+    slider.classList.add(ds);
 
-    prevArrow.classList.add(`swiper-button-prev-${index}`)
-    nextArrow.classList.add(`swiper-button-next-${index}`)
-
+    prevArrow.classList.add(`swiper-button-prev-${index}`);
+    nextArrow.classList.add(`swiper-button-next-${index}`);
 
     const swiper = new Swiper(`.${ds}`, {
       autoHeight: true,
@@ -21,16 +20,15 @@
         el: ".swiper-pagination",
         type: "custom",
         renderCustom: function (swiper, current, total) {
-          return current + ' из ' + total; 
-        }
+          return current + " из " + total;
+        },
       },
       navigation: {
         nextEl: nextArrow,
         prevEl: prevArrow,
       },
-    })
-  })
-  
+    });
+  });
 })();
 
 if (document.querySelector(".ui-page")) {
@@ -44,31 +42,77 @@ const Museum = {
   headerHeight: 0,
 
   updateHeight: () => {
-    this.headerHeight = document.querySelector('.museum-header').clientHeight
+    this.headerHeight = document.querySelector(".museum-header").clientHeight;
   },
 
   scroll: () => {
     if (window.scrollY >= this.headerHeight) {
-      return document.querySelector("#header").classList.remove("header-transparent")
+      return document
+        .querySelector("#header")
+        .classList.remove("header-transparent");
     }
-    document.querySelector("#header").classList.add("header-transparent")
-  }
-}
+    document.querySelector("#header").classList.add("header-transparent");
+  },
+};
 
 if (document.querySelector(".museum")) {
-  document.querySelector(".wrapper").classList.add("wrapper-museum")
-  document.querySelector("#header").classList.add("header-transparent")
-  
-  Museum.updateHeight()
+  document.querySelector(".wrapper").classList.add("wrapper-museum");
+  document.querySelector("#header").classList.add("header-transparent");
 
-  window.addEventListener('resize',Museum.updateHeight,false)
+  Museum.updateHeight();
 
-  window.addEventListener('scroll',Museum.scroll,false)
+  window.addEventListener("resize", Museum.updateHeight, false);
+
+  window.addEventListener("scroll", Museum.scroll, false);
 }
 
+// График  company.html
 
-// if (window.pageYOffset > 0) {
-//   let wrap = document.querySelector(".wrapper");
-//   wrap.classList.remove("ui-wrapper");
-//   document.querySelector(".header").style.background = "#222E3F";
-// }
+const ctx = document.getElementById("company_chart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ["#F08322"],
+        hoverBackgroundColor: ["black"],
+        borderColor: ["white"],
+        borderWidth: 1,
+        datalabels: {
+          color: "blue",
+        },
+      },
+    ],
+  },
+  // plugins: [ChartDataLabels],
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          // display: false,
+          drawBorder: false,
+          color: "transparent",
+        },
+        ticks: {
+          color: "transparent",
+        },
+      },
+      x: {
+        beginAtZero: true,
+        grid: {
+          // display: false,
+          drawBorder: false,
+          color: "transparent",
+        },
+        ticks: {
+          color: "#DD722A",
+        },
+      },
+    },
+    onHover: {},
+  },
+});
