a99ecc7da74f8398b18fb649c207c81c415394c0
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-02-26 12:46:32 +0100

    qwe
    qwe

--- src/scripts/dima.js
+++ src/scripts/dima.js
@@ -116,3 +116,18 @@ const myChart = new Chart(ctx, {
     onHover: {},
   },
 });
+
+// Плавный скролл
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
