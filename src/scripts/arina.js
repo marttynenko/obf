686e28fd7a0608723b7da7da3d9e5ac822be696c
arichild <arieevq@gmail.com>
2022-02-27 15:01:16 +0300

    add popup cookie

--- src/scripts/arina.js
+++ src/scripts/arina.js
@@ -8,3 +8,35 @@ function loader() {
   preloader.classList.add('disppear');
   scrollbar.classList.remove('hidden');
 }
+
+//popup cookie 
+const popUp = document.getElementById("cookiePopup");
+const btnAcceptCookie = document.getElementById("acceptCookie");
+
+btnAcceptCookie.addEventListener("click", () => {
+  let d = new Date();
+
+  d.setMinutes(2 + d.getMinutes());
+  document.cookie = "myCookieName=thisIsMyCookie; expires = " + d + ";";
+
+  popUp.classList.add("hide");
+  popUp.classList.remove("show");
+});
+
+const checkCookie = () => {
+  const input = document.cookie.split("=");
+
+  if (input[0] == "myCookieName") {
+    popUp.classList.add("hide");
+    popUp.classList.remove("show");
+  } else {
+    popUp.classList.add("show");
+    popUp.classList.remove("hide");
+  }
+};
+
+window.onload = () => {
+  setTimeout(() => {
+    checkCookie();
+  }, 2000);
+};
